2d2bb36180a204e7b3156209391598c6ceaaa57d
Tom <73348376+tom-devv@users.noreply.github.com>
2024-02-04 21:20:04 +0000

    threading attempt

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,15 @@
 mod packet;
 
-use std::net::{IpAddr, SocketAddr, TcpStream};
+use std::error::Error;
+use std::thread;
+use std::net::{IpAddr};
 use std::fs::File;
-use std::io;
-use std::io::{BufRead, BufReader, Cursor, Read, Write};
+use std::io::{BufRead, BufReader, Read, Write};
 use std::str::FromStr;
-use byteorder::{BigEndian, WriteBytesExt};
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
+use mcping::{get_status, Response};
 
-
-struct MinecraftServer {
-    ip: IpAddr,
-}
-
-struct TCPConnection {
-    stream: TcpStream,
-    socket: SocketAddr,
-}
-
-struct Packet {
-    handshake: SocketAddr
-}
-
-impl Packet {
-    fn send(&self) {
-
-    }
-}
 
 fn parse_ip(line: String) -> Result<IpAddr, &'static str> {
     let slice = line.split_whitespace();
@@ -55,24 +39,56 @@ fn read_ips(file_path: &str)-> Result<Vec<IpAddr>, Box<dyn std::error::Error>> {
     Ok(ips)
 }
 
+fn write_file(ip: &str) -> std::io::Result<()> {
 
-fn handshake(socket: SocketAddr) -> io::Result<()> {
-    println!("{}", socket);
-    let mut tcp_stream = TcpStream::connect((socket.ip(), socket.port())).expect("Failed to connect to socket");
+    let mut file = File::create("active.txt")?;
 
-    let mut handshake_packet = Vec::new();
+    file.write_all(ip.as_bytes())?;
 
-
+    println!("Wrote {} -> to file", ip);
 
     Ok(())
-
-
 }
 
-fn main() {
-    // let addr = read_ips("masscan.txt").expect("Failed to read IP's");
-    // handshake(SocketAddr::from_str("188.40.22.42:25503").unwrap());
-    // let (ping, response) = get_status("188.40.22.42:25503", None).unwrap();
-    // println!("Ping {}, Response {:?}", ping, response.players.online);
+
+
+
+
+fn main()  {
+    let ips = read_ips("ips.txt").unwrap();
+    let active_ips: Arc<Mutex<Vec<Response>>> = Arc::new(Mutex::new(Vec::new()));
+    let mut handles = Vec::new();
+
+    for ip in ips {
+        let active_ips_clone = Arc::clone(&active_ips);
+
+        // Spawn many threads here
+        let handle = thread::spawn(move || {
+            println!("Contacting {}", ip);
+            let status = get_status(&ip.to_string(), Duration::from_secs(2));
+            match status {
+                Ok((_, res)) => {
+                    // Lock the mutex before modifying the shared vector
+                    let mut active_ips = active_ips_clone.lock().unwrap();
+                    active_ips.push(res);
+                }
+                Err(err) => {
+                    eprintln!("Error contacting ip, {}", err);
+                }
+            }
+        });
+        handles.push(handle);
+
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+// Do this after all threads have closed
+    let active_ips = active_ips.lock().unwrap();
+    for active_ip in active_ips.iter() {
+        write_file(&active_ip.version.name).expect("TODO: panic message");
+    }
 }
 
